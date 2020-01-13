267a5b8042d322025eab508710006d3562292522
James Mba Azam <jamesazam@sun.ac.za>
2020-01-13 12:48:34 +0200

    added the report factory templates repo files

--- assignment1/scripts/assignment1.R
+++ assignment1/scripts/assignment1.R
@@ -2,8 +2,12 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(here)
+library(incidence)
 
 set_here()
 ebola_dat <- read_csv(file = './assignment1/data/ebola_1.csv')
 
 View(ebola_dat)
+
+ebola_inc <- incidence(ebola_dat$onsetDate, groups = ebola_dat$status)
+plot(ebola_inc, xlab = 'Date of onset')
